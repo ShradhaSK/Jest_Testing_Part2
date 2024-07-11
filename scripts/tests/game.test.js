434422c7b44403c5a7a2003821ce2af491f5543f
ShradhaSK <131806140+ShradhaSK@users.noreply.github.com>
2024-07-11 10:37:40 +0200

    Add first failing test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,4 +2,18 @@
  * @jest-environment jsdom
  */
 
+const { describe } = require("node:test");
 
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe('game object contains correct keys', () => {
+    test('score key exists', () => {
+        expect('score' in game).toBe(true);
+    });
+});
